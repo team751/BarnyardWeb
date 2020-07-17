95b2acb1ab6db6bcaaedc565cba67d6ff05c02c6
Bobby Youstra <bobbyyoustra@protonmail.com>
2020-07-17 12:03:25 -0700

    Add google signon test

--- barnyard/index.js
+++ barnyard/index.js
@@ -1,9 +1,24 @@
 function checkLogin() {
+    console.log("loaded")
+
     if(!navigator.cookieEnabled) {
         $('alert-placeholder').html('<div id="cookiealert" class="alert alert-warning">' +
                                     '<span>Cookies aren\'t enabled! Please enable them to use '+
                                     'Barnyard-Web</span></div>')
     }
 
+    const auth2 = gapi.auth2.getAuthInstance();
 
+    if(auth2.isSignedIn.get()) {
+        onSignIn(auth2.currentUser.get())
+    } else {
+        $('alert-placeholder').html('<div id="cookiealert" class="alert alert-warning">' +
+                                    '<span>Sign in with google to proceed.</span></div>')
+
+        $('google-sign-in').html('<div class="g-signin2" data-onsuccess="onSignIn"></div>')
+    }
+}
+
+function onSignIn(googleUser){
+    //
 }
